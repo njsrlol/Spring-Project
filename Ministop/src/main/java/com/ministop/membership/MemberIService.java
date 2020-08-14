3bca7c16ed1417c0b5190ebb513a5d3d9c4b9fa8
jsPark <dduruwjdtn@gmail.com>
2020-08-14 16:00:49 +0900

    project start

---
+++ Ministop/src/main/java/com/ministop/membership/MemberIService.java
@@ -0,0 +1,14 @@
+package com.ministop.membership;
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface MemberIService {
+	public String isExistID(Member memberq, Map<String, Object> sInfo);
+	public void reqAuthNum(Map<String, Object> sInfo);
+	public String authNumConfirm(Map<String, Object> sInfo, String reqAuthNum);
+	public boolean memberProc(Member memberq, Map<String, Object> sInfo);
+	public String findInfoProc(String email, String id);
+	public boolean loginProc(Login login);
+}
